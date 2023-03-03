c5c5dda87877c660136d64dbfc0eb456c7875532
Smit Valture <smitvalture@gmail.com>
2023-03-03 11:15:25 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import Landing from "./pages/Landing";
-import Header from "./pages/Header";
+
 
 function App() {
   return (
-    <div className="max-w-[1240px] mx-auto">
-      <Header />
+    <div className="">
+      
       <Landing />
     </div>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,29 +1,17 @@
-import mission from './images/our_mission_img.svg'
+import Header from './Header'
+
+import Mission from './Mission'
 
 const Landing = () => {
 
 
     return (
         <div className=''>
-            {/* navbar */}
+            <Header />
+            <Mission />
             
 
-            <section id="mission" className="text-[#15213A] flex flex-col justify-center items-center">
-                {/* <h1 className="text-2xl lap:text-4xl font-semibold mt-16 tab:mt-20 lap:mt-24">
-                    Our mission
-                </h1> */}
-                <div className="flex flex-col tab:flex-row lap:flex-row items-center justify-between mt-20 lap:mt-24 mb-20 lap:mb-40">
-                    <p className="text-center tab:text-left w-80 lap:w-[543px] text-[15px] lap:text-xl font-normal">
-                        Our mission is to empower businesses to streamline their payroll
-                        processes and reduce costs through innovative solutions and
-                        exceptional customer service. We strive to simplify the payroll
-                        process and provide unparalleled value to our clients, enabling them
-                        to focus on their core business objectives while achieving financial
-                        stability and growth.
-                    </p>
-                    <img className="w-80 h-80 lap:w-96 lap:h-96 mt-16 tab:mt-auto lap:ml-20" src={mission} alt="" />
-                </div>
-            </section>
+            
         </div>
     )
 }
