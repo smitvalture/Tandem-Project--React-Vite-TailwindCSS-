4e9e2d99865f6893b5ab1a02a032ca833f595c5d
Smit Valture <smitvalture@gmail.com>
2023-03-07 16:36:56 +0530

    ss

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -3,8 +3,8 @@ import { MdWork, MdLocationPin } from "react-icons/md";
 
 const Section2 = () => {
     return (
-        <div className='w-full h-[calc(100vh-250px)] tab:h-[calc(100vh-200px)] lap:h-[calc(100vh-160px)] flex justify-center items-center'>
-            <div className='space-y-14 tab:space-y-20 lap:space-y-28 mb-40'>
+        <div className='w-full h-[calc(100vh-250px)] tab:h-[calc(100vh-200px)] lap:h-[calc(100vh-160px)]'>
+            <div className='space-y-14 tab:space-y-20 lap:space-y-28 mb-40 flex flex-col justify-center items-center'>
                 <h1 className='text-center text-5xl tab:text-7xl h-20 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500'>Job Search</h1>
                 <div className='bg-white w-[350px] tab:w-[580px] lap:w-[800px] flex flex-col lap:flex-row justify-between items-center px-10 py-20 rounded-xl shadow-lg space-y-5 lap:space-y-0'>
                     <div className='flex flex-col tab:flex-row space-y-5 tab:space-y-0 tab:space-x-5 lap:mr-5 w-full'>
@@ -12,7 +12,7 @@ const Section2 = () => {
                             <MdWork />
                             <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="text" placeholder='Job Title' name="job" />
                         </div>
-                        <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 rounded-md w-full hover:border-cyan-500'>
+                        <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 hover:bg-gray-300 rounded-md w-full hover:border-cyan-500'>
                             <MdLocationPin />
                             <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="Search" placeholder='Location' name="location" />
                         </div>
