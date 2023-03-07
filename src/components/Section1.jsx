4d172a1f88313617f7d76460bbb3aac22c309e9e
Smit Valture <smitvalture@gmail.com>
2023-03-07 16:39:13 +0530

    ss

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -20,32 +20,34 @@ const Section1 = () => {
     }, []);
 
     return (
-        <div className='w-full h-[calc(100vh-70px)] tab:h-[calc(100vh-75px)] lap:h-[calc(100vh-80px)] flex justify-center items-center'>
-            <div className='max-w-[1240px] lap:p-5 flex flex-col lap:flex-row place-content-center place-items-center space-y-8 lap:space-y-0'>
-                <div className='lap:space-y-32'>
-                    <div className='flex flex-col items-center lap:items-start mr-5 w-full'>
-                        <h1 className='text-5xl tab:text-6xl h-20 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500'>High Paying Job</h1>
-                        <p className='mt-5 text-base tab:text-lg lap:text-xl lap:text-left text-center w-[350px] tab:w-[480px] lap:w-[520px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime explicabo ipsa iusto eos provident magni eligendi nesciunt quae rerum, consectetur dolor? Fugit, rem ipsa ex ratione soluta veritatis? Optio, magnam.</p>
+        <section>
+            <div className='w-full h-[calc(100vh-70px)] tab:h-[calc(100vh-75px)] lap:h-[calc(100vh-80px)] flex justify-center items-center'>
+                <div className='max-w-[1240px] lap:p-5 flex flex-col lap:flex-row place-content-center place-items-center space-y-8 lap:space-y-0'>
+                    <div className='lap:space-y-32'>
+                        <div className='flex flex-col items-center lap:items-start mr-5 w-full'>
+                            <h1 className='text-5xl tab:text-6xl h-20 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500'>High Paying Job</h1>
+                            <p className='mt-5 text-base tab:text-lg lap:text-xl lap:text-left text-center w-[350px] tab:w-[480px] lap:w-[520px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime explicabo ipsa iusto eos provident magni eligendi nesciunt quae rerum, consectetur dolor? Fugit, rem ipsa ex ratione soluta veritatis? Optio, magnam.</p>
+                        </div>
+                        <div className='flex flex-row items-center justify-center lap:justify-start space-x-5 mt-8 lap:mt-16'>
+                            <button className='px-4 py-1 bg-blue-900 hover:bg-blue-800 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 text-white rounded-full text-xl w-36 h-fit'>Get Started</button>
+                            <button className='px-4 py-1 border border-1 border-blue-900 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 rounded-full text-xl w-36 h-fit'>Login</button>
+                        </div>
                     </div>
-                    <div className='flex flex-row items-center justify-center lap:justify-start space-x-5 mt-8 lap:mt-16'>
-                        <button className='px-4 py-1 bg-blue-900 hover:bg-blue-800 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 text-white rounded-full text-xl w-36 h-fit'>Get Started</button>
-                        <button className='px-4 py-1 border border-1 border-blue-900 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 rounded-full text-xl w-36 h-fit'>Login</button>
-                    </div>
-                </div>
 
-                <div className='w-full'>
-                    <video
-                        playsInline
-                        loop
-                        muted
-                        alt="All the devices"
-                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-                        ref={videoEl}
-                        className='rounded-lg w-[350px] tab:w-[480px] lap:w-[520px] mx-auto'
-                    />
+                    <div className='w-full'>
+                        <video
+                            playsInline
+                            loop
+                            muted
+                            alt="All the devices"
+                            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                            ref={videoEl}
+                            className='rounded-lg w-[350px] tab:w-[480px] lap:w-[520px] mx-auto'
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -3,26 +3,28 @@ import { MdWork, MdLocationPin } from "react-icons/md";
 
 const Section2 = () => {
     return (
-        <div className='w-full h-[calc(100vh-250px)] tab:h-[calc(100vh-200px)] lap:h-[calc(100vh-160px)]'>
-            <div className='space-y-14 tab:space-y-20 lap:space-y-28 mb-40 flex flex-col justify-center items-center'>
-                <h1 className='text-center text-5xl tab:text-7xl h-20 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500'>Job Search</h1>
-                <div className='bg-white w-[350px] tab:w-[580px] lap:w-[800px] flex flex-col lap:flex-row justify-between items-center px-10 py-20 rounded-xl shadow-lg space-y-5 lap:space-y-0'>
-                    <div className='flex flex-col tab:flex-row space-y-5 tab:space-y-0 tab:space-x-5 lap:mr-5 w-full'>
-                        <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 hover:bg-gray-300 rounded-md w-full hover:border-cyan-500'>
-                            <MdWork />
-                            <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="text" placeholder='Job Title' name="job" />
-                        </div>
-                        <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 hover:bg-gray-300 rounded-md w-full hover:border-cyan-500'>
-                            <MdLocationPin />
-                            <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="Search" placeholder='Location' name="location" />
-                        </div>
+        <section>
+            <div className='w-full h-[calc(100vh-250px)] tab:h-[calc(100vh-200px)] lap:h-[calc(100vh-160px)]'>
+                <div className='space-y-14 tab:space-y-20 lap:space-y-28 mb-40 flex flex-col justify-center items-center'>
+                    <h1 className='text-center text-5xl tab:text-7xl h-20 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500'>Job Search</h1>
+                    <div className='bg-white w-[350px] tab:w-[580px] lap:w-[800px] flex flex-col lap:flex-row justify-between items-center px-10 py-20 rounded-xl shadow-lg space-y-5 lap:space-y-0'>
+                        <div className='flex flex-col tab:flex-row space-y-5 tab:space-y-0 tab:space-x-5 lap:mr-5 w-full'>
+                            <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 hover:bg-gray-300 rounded-md w-full hover:border-cyan-500'>
+                                <MdWork />
+                                <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="text" placeholder='Job Title' name="job" />
+                            </div>
+                            <div className='flex items-center bg-gray-100 px-2 py-1 shadow-lg border border-1 border-gray-500 hover:bg-gray-300 rounded-md w-full hover:border-cyan-500'>
+                                <MdLocationPin />
+                                <input className='bg-gray-100 outline-none ml-2 w-full bg-transparent hover:bg-transparent' type="Search" placeholder='Location' name="location" />
+                            </div>
 
 
+                        </div>
+                        <button className='px-4 py-1 bg-blue-900 hover:bg-blue-800 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 text-white rounded-full w-36 h-fit' type="submit">Search</button>
                     </div>
-                    <button className='px-4 py-1 bg-blue-900 hover:bg-blue-800 shadow-lg shadow-blue-400 hover:shadow-blue-400/70 text-white rounded-full w-36 h-fit' type="submit">Search</button>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
